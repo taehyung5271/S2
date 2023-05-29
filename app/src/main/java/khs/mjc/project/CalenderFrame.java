082abd6180c0da5da346bd036746d2a31a3d307a
SeungBeakHong <ghdtmdqor96@gmail.com>
2023-05-29 18:56:04 +0900

    Update CalenderFrame.java

--- app/src/main/java/khs/mjc/project/CalenderFrame.java
+++ app/src/main/java/khs/mjc/project/CalenderFrame.java
@@ -1,5 +1,8 @@
 package khs.mjc.project;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,9 +11,13 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.CalendarView;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.util.Calendar;
 
 /**
@@ -19,16 +26,17 @@ import java.util.Calendar;
  * create an instance of this fragment.
  */
 public class CalenderFrame extends Fragment {
+    public String fname = null;
+    public String str = null;
     private TextView monthTextView;
     private CalendarView calendarView;
+    public Button cha_Btn, del_Btn, save_Btn;
+    public TextView textView2;
+    public EditText contextEditText;
     TextView monthname;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
@@ -36,15 +44,6 @@ public class CalenderFrame extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CalenderFrame.
-     */
-    // TODO: Rename and change types and number of parameters
     public static CalenderFrame newInstance(String param1, String param2) {
         CalenderFrame fragment = new CalenderFrame();
         Bundle args = new Bundle();
@@ -66,35 +65,155 @@ public class CalenderFrame extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_calender_frame, container, false);
         monthTextView = v.findViewById(R.id.mon_text);
         calendarView = v.findViewById(R.id.calendarView);
-        monthname= v.findViewById(R.id.mon_nametext);
+        monthname = v.findViewById(R.id.mon_nametext);
+        save_Btn = v.findViewById(R.id.save_Btn);
+        del_Btn = v.findViewById(R.id.del_Btn);
+        cha_Btn = v.findViewById(R.id.cha_Btn);
+        textView2 = v.findViewById(R.id.textView2);
+        contextEditText = v.findViewById(R.id.contextEditText);
+
+        //로그인 식별코드를 받아옴
+        Intent intent = getActivity().getIntent();
+        String name = intent.getStringExtra("userName");
+        final String userID = intent.getStringExtra("userID");
 
         Calendar calendar = Calendar.getInstance();
         calendar.setTimeInMillis(calendarView.getDate());
         int currentMonth = calendar.get(Calendar.MONTH) + 1;
         monthTextView.setText(String.valueOf(currentMonth));
-        String month_[] = {"january", "february","march",
-                "april", "may", "june","july","august", "september",
-                "october", "november", "december"}; //월 저장
-        // 캘린더뷰에 날짜 선택 리스너 설정
+        String month_[] = {"january", "february", "march",
+                "april", "may", "june", "july", "august", "september",
+                "october", "november", "december"};
+
         calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
-                // 선택된 날짜 정보를 로그에 출력
                 String selectedDate = year + "-" + (month + 1) + "-" + dayOfMonth;
                 Log.d("MainActivity", "Selected date: " + selectedDate);
 
-                // 선택된 날짜의 월을 텍스트뷰에 표시
                 String selectedMonthStr = String.valueOf(month + 1);
                 monthTextView.setText(selectedMonthStr);
                 monthname.setText(month_[month]);
+
+                save_Btn.setVisibility(View.VISIBLE);
+                contextEditText.setVisibility(View.VISIBLE);
+                textView2.setVisibility(View.INVISIBLE);
+                cha_Btn.setVisibility(View.INVISIBLE);
+                del_Btn.setVisibility(View.INVISIBLE);
+                contextEditText.setText("");
+                checkDay(year, month, dayOfMonth, userID);
             }
         });
 
+        save_Btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveDiary(fname);
+                str = contextEditText.getText().toString();
+                textView2.setText(str);
+                save_Btn.setVisibility(View.INVISIBLE);
+                cha_Btn.setVisibility(View.VISIBLE);
+                del_Btn.setVisibility(View.VISIBLE);
+                contextEditText.setVisibility(View.INVISIBLE);
+                textView2.setVisibility(View.VISIBLE);
+            }
+        });
+//이후 버전
 
         return v;
     }
+
+    public void checkDay(int cYear, int cMonth, int cDay, String userID) {
+        fname = "" + userID + cYear + "-" + (cMonth + 1) + "" + "-" + cDay + ".txt";
+        FileInputStream fis = null;
+
+        try {
+            fis = getActivity().openFileInput(fname);
+
+            byte[] fileData = new byte[fis.available()];
+            fis.read(fileData);
+            fis.close();
+
+            str = new String(fileData);
+
+            contextEditText.setVisibility(View.INVISIBLE);
+            textView2.setVisibility(View.VISIBLE);
+            textView2.setText(str);
+
+            save_Btn.setVisibility(View.INVISIBLE);
+            cha_Btn.setVisibility(View.VISIBLE);
+            del_Btn.setVisibility(View.VISIBLE);
+
+            cha_Btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    contextEditText.setVisibility(View.VISIBLE);
+                    textView2.setVisibility(View.INVISIBLE);
+                    contextEditText.setText(str);
+
+                    save_Btn.setVisibility(View.VISIBLE);
+                    cha_Btn.setVisibility(View.INVISIBLE);
+                    del_Btn.setVisibility(View.INVISIBLE);
+                    textView2.setText(contextEditText.getText());
+                }
+            });
+
+            del_Btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    textView2.setVisibility(View.INVISIBLE);
+                    contextEditText.setText("");
+                    contextEditText.setVisibility(View.VISIBLE);
+                    save_Btn.setVisibility(View.VISIBLE);
+                    cha_Btn.setVisibility(View.INVISIBLE);
+                    del_Btn.setVisibility(View.INVISIBLE);
+                    removeDiary(fname);
+                }
+            });
+
+            if (textView2.getText() == null) {
+                textView2.setVisibility(View.INVISIBLE);
+                save_Btn.setVisibility(View.VISIBLE);
+                cha_Btn.setVisibility(View.INVISIBLE);
+                del_Btn.setVisibility(View.INVISIBLE);
+                contextEditText.setVisibility(View.VISIBLE);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @SuppressLint("WrongConstant")
+    public void removeDiary(String readDay) {
+        FileOutputStream fos = null;
+
+        try {
+            fos = getActivity().openFileOutput(readDay, Context.MODE_PRIVATE);
+            String content = "";
+            fos.write(content.getBytes());
+            fos.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void saveDiary(String readDay) {
+        FileOutputStream fos = null;
+
+        try {
+            fos = getActivity().openFileOutput(readDay, Context.MODE_PRIVATE);
+            String content = contextEditText.getText().toString();
+            fos.write(content.getBytes());
+            fos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
+
